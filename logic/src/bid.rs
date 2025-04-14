8dcee559d00fce6771196d3a042899c09601d959
Miraculous Owonubi <omiraculous@gmail.com>
2025-04-14 05:02:43 +0100

    create_bid

--- logic/src/bid.rs
+++ logic/src/bid.rs
@@ -41,3 +41,56 @@ pub enum BidStatus {
     Approved,
     Retracted { reason: Option<String> },
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(crate = "calimero_sdk::serde")]
+pub struct CreateBidRequest {
+    pub brief: String,
+    pub bounty: BountyId,
+    pub expiry: Option<u64>,
+    pub reward: BTreeMap<String, u128>,
+    pub duration: Option<u64>,
+}
+
+#[app::logic]
+impl AppState {
+    pub fn create_bid(&mut self, request: CreateBidRequest) -> app::Result<BidId> {
+        let user_id = self.current_user();
+
+        let mut user = self.get_registered_user(&user_id)?;
+
+        let bid_id = unique(|| BidId::random(), |id| self.bids.contains(id))?;
+
+        let message_id = self.internal_post_message(
+            user_id,
+            &mut user,
+            MessageTarget::Bid(bid_id),
+            request.brief,
+        )?;
+
+        let bid = Bid {
+            author: user_id,
+            bounty: request.bounty,
+
+            message: message_id,
+
+            assignment: None,
+
+            status: BidStatus::Proposed,
+            expiry: request.expiry,
+            reward: request.reward.into_iter().collect(),
+            duration: request.duration,
+
+            proposed_at: None,
+            approved_at: None,
+            retracted_at: None,
+        };
+
+        let _ignored = user.bids.insert(bid_id)?;
+
+        let _ignored = self.users.insert(user_id, user)?;
+        let _ignored = self.bids.insert(bid_id, bid)?;
+
+        Ok(bid_id)
+    }
+}
